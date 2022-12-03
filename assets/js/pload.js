0a981bc10cfb8d22537f4082dbddd2ee4400ed0c
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-12-03 19:35:37 +0530

    Update pload.js

--- assets/js/pload.js
+++ assets/js/pload.js
@@ -1,1 +1,7 @@
-hehe
+const fadeOut = () => {
+  const loaderWrapper.classList.add('fade');
+  document.querySelector('.wrapper');
+  loaderWrapper.classList.add('fade');
+}
+
+window.addEventListener('load', fadeOut);
